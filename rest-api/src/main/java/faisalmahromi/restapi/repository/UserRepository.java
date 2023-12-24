2bcbcd447cc59a05d37b61c9e5c0b608aeb30f98
faisal-t <faizalmahromi35@gmail.com>
2023-12-25 04:04:53 +0700

    implement `register-api` and unit test

---
+++ rest-api/src/main/java/faisalmahromi/restapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package faisalmahromi.restapi.repository;
+
+import faisalmahromi.restapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
